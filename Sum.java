2352356d168e07e6844cd53de49b231cae845566
Nithin017 <73475105+Nithin1728@users.noreply.github.com>
2022-07-10 18:15:34 +0530

    Add files via upload
    Sum of 2 numbers program

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
