250b52bf24756982d2e012958a378ab9bc3d50b3
Nithin017 <73475105+Nithin1728@users.noreply.github.com>
2022-07-25 22:47:13 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+
+public class loops{
+    public static void main(String[] args) {
+//        if(true){
+//            System.out.println("hello");
+//        }
+
+
+//        int count = 1;
+//        while(count==1){
+//            System.out.println(count);
+//            count++;
+//
+//        }
+
+        for (int i=1; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
