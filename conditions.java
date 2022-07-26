12ca5c479174028f603180002ea9f5b1f9447dcd
Nithin017 <73475105+Nithin1728@users.noreply.github.com>
2022-07-26 20:07:24 +0530

    Add files via upload

---
+++ conditions.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+
+public class conditions {
+
+    public static void main(String[] args) {
+        /*
+        syntax for conditions is if(boolean expression T or F){
+        //body
+
+        } else {
+        // do this
+
+
+        }
+
+         */
+        Scanner in = new Scanner(System.in);
+        int salary = in.nextInt();
+
+        if(salary > 10000) {
+            System.out.println("your salary pay has increased by 2000");
+            int salary2 = salary + 2000;
+            System.out.println(salary2);
+        }
+        else{
+            System.out.println("your salary is the same " + salary);
+        }
+    }
+
+
+
+}
