0f18dd87995556fe9ae4cfe9801132090ec62fa0
Nithin017 <73475105+Nithin1728@users.noreply.github.com>
2022-07-27 20:56:39 +0530

    Add files via upload

---
+++ casecheck.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class casecheck {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+
+        if(ch>='a' && ch<='z'){
+            System.out.println("lower");
+
+        }else {
+            System.out.println("upper");
+        }
+    }
+
+}
