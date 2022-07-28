88ca3aa6a6fb17cbde7b5180a687f3e1d5cb6a66
Nithin017 <73475105+Nithin1728@users.noreply.github.com>
2022-07-28 13:08:49 +0530

    Add files via upload

---
+++ areaofcircle.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+
+public class areaofcircle {
+
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+        int r = in.nextInt();
+        double pi = 3.141592653589793238;
+        double A = pi*(r*r);
+        System.out.println(A);
+
+
+
+    }
+
+}
