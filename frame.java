aafccef4e40d363292aecb64974d4807fe3dcefa
Nithin017 <73475105+Nithin1728@users.noreply.github.com>
2022-08-05 20:52:36 +0530

    Add files via upload

---
+++ frame.java
@@ -0,0 +1,15 @@
+//creating a gui frame in java
+
+import java.util.*;
+import java.awt.*;
+
+public class frame {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setVisible(true);
+        f.setSize(300,300);
+        f.setBackground(Color.BLACK);
+    }
+
+
+}
